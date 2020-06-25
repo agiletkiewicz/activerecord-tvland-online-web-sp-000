a5d6b2edebc928a6dcd1dd9657a7fdce3be991bc
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-25 03:14:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,8 @@ class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :network
   
+  def actors_list 
+    self.characters.map {|character| "#{character.actor.first_name} #{character.actor.last_name}"}
+  end
+  
 end
